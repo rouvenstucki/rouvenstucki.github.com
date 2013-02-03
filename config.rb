b367493da565314341f436baaa136763af7d74db
Leander Herzog <info@leanderherzog.ch>
2013-02-03 19:57:16 +0100

    cleanup

--- config.rb
+++ config.rb
@@ -1,10 +1,9 @@
 require "compass-h5bp"
-# Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css/"
-sass_dir = "src"
+css_dir = "css"
+sass_dir = "css"
 images_dir = "images"
 javascripts_dir = "js"
 output_style = :compressed
